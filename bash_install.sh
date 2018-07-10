5b188ca728cab59cacce0f40ed79a3dbcd1fde9e
baflo <834350+baflo@users.noreply.github.com>
2018-07-10 17:01:46 +0200

    Fixed error "[: neovim: binary operator expected"

--- bash_install.sh
+++ bash_install.sh
@@ -44,10 +44,10 @@ then
     POST_INSTALL_CMDS+=("curl -L https://rawgit.com/pimterry/notes/latest-release/install.sh | bash && mkdir -p ~/.config/notes && curl_nc_tf $BASH_CONFIG_GIT_PATH/notes_config ~/.config/notes/config && curl https://cdn.rawgit.com/pimterry/notes/latest-release/notes.bash_completion | sudo tee /usr/share/bash-completion/completions/notes > /dev/null")
 fi
 
-if [ ! -z ${PRE_UPDATE_PKGS[0]}  ] ; then sudo apt-get -y install ${PRE_UPDATE_PKGS[@]}; fi
-if [ ! -z ${PRE_UPDATE_REPOS[0]} ] ; then sudo add-apt-repository -y ${PRE_UPDATE_REPOS[@]}; fi
-if [ ! -z ${PRE_UPDATE_REPOS[0]} ] ; then sudo apt-get -y update; fi
-if [ ! -z ${POST_UPDATE_PKGS[0]} ] ; then sudo apt-get -y install ${POST_UPDATE_PKGS[@]}; fi
+if [ ! -z "${PRE_UPDATE_PKGS[0]}"  ] ; then sudo apt-get -y install ${PRE_UPDATE_PKGS[@]}; fi
+if [ ! -z "${PRE_UPDATE_REPOS[0]}" ] ; then sudo add-apt-repository -y ${PRE_UPDATE_REPOS[@]}; fi
+if [ ! -z "${PRE_UPDATE_REPOS[0]}" ] ; then sudo apt-get -y update; fi
+if [ ! -z "${POST_UPDATE_PKGS[0]}" ] ; then sudo apt-get -y install ${POST_UPDATE_PKGS[@]}; fi
 
 for cmd in "${POST_INSTALL_CMDS[@]}"
 do
